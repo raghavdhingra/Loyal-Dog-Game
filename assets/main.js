7cc0099888c2416707b3ebfcab346873639c9b9d
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-05-12 22:15:08 +0530

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+const text = document.getElementById("text");
+const box = document.getElementById("box");
+const human = document.getElementById("human");
+const play_1 = document.getElementById("play_1");
+for (i=0;i<=500;i++){
+    val1 = Math.random();
+    elmt.style.top = `${ran1}%`;
+    elmt.style.left = `${ran2}%`;
+    document.getElementById("play_1").appendChild(elmt);
+}
